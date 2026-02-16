2ce83cb0534c893487a01e5186755fcb9c2d7fb1
Pranav A <pranavarunkumaraj@gmail.com>
2026-02-16 07:36:14 +0530

    Added Leaderboard

--- LabAssignment2.py
+++ LabAssignment2.py
@@ -21,10 +21,8 @@ mass_to_aa = {
 
 specC = Counter(spectrum)
 
-
 def mass(p):
     return sum(p)
-
 
 def linear(p):
     s = [0]
@@ -35,66 +33,99 @@ def linear(p):
             s.append(t)
     return s
 
-
 def cyclic(p):
     s = [0]
     n = len(p)
     d = p+p
-
     for l in range(1,n):
         for i in range(n):
             s.append(sum(d[i:i+l]))
-
     s.append(sum(p))
     return sorted(s)
 
-
-def ok(p):
-    c = Counter(linear(p))
-    for x in c:
-        if c[x] > specC[x]:
-            return False
-    return True
-
+def score(peptide, spectrum, cyclic_mode=False):
+    if cyclic_mode:
+        theo = Counter(cyclic(peptide))
+    else:
+        theo = Counter(linear(peptide))
+    exp = Counter(spectrum)
+    return sum(min(theo[x], exp[x]) for x in theo)
 
 def expand(S):
-    return {p+(a,) for p in S for a in amino}
-
+    return [p+(a,) for p in S for a in amino]
 
 def solve():
-
     cand = {()}
     res = set()
+    target = sorted(spectrum)
 
     while cand:
-
-        cand = expand(cand)
+        cand = {p+(a,) for p in cand for a in amino}
         new = set()
 
         for p in cand:
-
             m = mass(p)
 
             if m == parent:
-
-                if cyclic(list(p)) == sorted(spectrum):
+                if cyclic(p) == target:
                     res.add(p)
 
-            elif m < parent and ok(list(p)):
-                new.add(p)
+            elif m < parent:
+                c = Counter(linear(p))
+                valid = True
+                for x in c:
+                    if c[x] > specC[x]:
+                        valid = False
+                        break
+                if valid:
+                    new.add(p)
 
         cand = new
 
     return res
 
+def trim(leaderboard, spectrum, N):
+    scored = [(p, score(p, spectrum)) for p in leaderboard]
+    scored.sort(key=lambda x: x[1], reverse=True)
+
+    if len(scored) <= N:
+        return [p for p, s in scored]
+
+    cutoff = scored[N-1][1]
+    return [p for p, s in scored if s >= cutoff]
+
+def leaderboard_cyclopeptide_sequencing(spectrum, N):
+    leaderboard = [()]
+    leader = ()
+    parent_mass = max(spectrum)
+
+    while leaderboard:
+        leaderboard = expand(leaderboard)
+        new_board = []
+
+        for peptide in leaderboard:
+            m = mass(peptide)
+
+            if m == parent_mass:
+                if score(peptide, spectrum, cyclic_mode=True) > score(leader, spectrum, cyclic_mode=True):
+                    leader = peptide
+
+            if m <= parent_mass:
+                new_board.append(peptide)
+
+        leaderboard = trim(new_board, spectrum, N)
+
+    return leader
 
 def to_letters(p):
     return "".join(mass_to_aa[x] for x in p)
 
+ans_branch = solve()
+ans_leader = leaderboard_cyclopeptide_sequencing(spectrum, 10)
 
-ans = solve()
-
-print("List of consistent 5-mers:\n")
-
-for p in sorted(ans):
+print("Branch and Bound Results:\n")
+for p in sorted(ans_branch):
     print(to_letters(p))
+
+print("\nLeaderboard Result:\n")
+print(to_letters(ans_leader))
