42b9d8f085a0308fcc5e9a86f366dd4c0ca2ce35
Pranav A <pranavarunkumaraj@gmail.com>
2026-02-16 07:25:35 +0530

    Branch and Bound

--- LabAssignment2.py
+++ LabAssignment2.py
@@ -5,6 +5,8 @@ spectrum = [
     295,297,299,299,301,394,396,398,400,400,497
 ]
 
+parent = max(spectrum)
+
 amino = [
     57,71,87,97,99,101,103,
     113,114,115,128,129,131,
@@ -18,30 +20,34 @@ mass_to_aa = {
 }
 
 specC = Counter(spectrum)
-parent = max(spectrum)
-target = sorted(spectrum)
+
 
 def mass(p):
     return sum(p)
+
 
 def linear(p):
     s = [0]
     for i in range(len(p)):
         t = 0
-        for j in range(i, len(p)):
+        for j in range(i,len(p)):
             t += p[j]
             s.append(t)
     return s
 
+
 def cyclic(p):
     s = [0]
     n = len(p)
-    d = p + p
-    for l in range(1, n):
+    d = p+p
+
+    for l in range(1,n):
         for i in range(n):
             s.append(sum(d[i:i+l]))
+
     s.append(sum(p))
     return sorted(s)
+
 
 def ok(p):
     c = Counter(linear(p))
@@ -50,73 +56,45 @@ def ok(p):
             return False
     return True
 
+
 def expand(S):
-    return {p + (a,) for p in S for a in amino}
+    return {p+(a,) for p in S for a in amino}
 
-def cyclopeptide_sequencing(Spectrum):
-    CandidatePeptides = {()}
-    FinalPeptides = set()
-    parent_mass = max(Spectrum)
-    SpectrumCounter = Counter(Spectrum)
-    SpectrumSorted = sorted(Spectrum)
-
-    while CandidatePeptides:
-        CandidatePeptides = {p + (a,) for p in CandidatePeptides for a in amino}
-        newCandidates = set()
-
-        for Peptide in CandidatePeptides:
-            m = sum(Peptide)
-
-            if m == parent_mass:
-                if sorted(cyclic(Peptide)) == SpectrumSorted:
-                    FinalPeptides.add(Peptide)
-
-            elif m < parent_mass:
-                c = Counter(linear(Peptide))
-                valid = True
-                for x in c:
-                    if c[x] > SpectrumCounter[x]:
-                        valid = False
-                        break
-                if valid:
-                    newCandidates.add(Peptide)
-
-        CandidatePeptides = newCandidates
-
-    return FinalPeptides
 
 def solve():
+
     cand = {()}
     res = set()
 
     while cand:
+
         cand = expand(cand)
         new = set()
 
         for p in cand:
+
             m = mass(p)
 
             if m == parent:
-                if cyclic(p) == target:
+
+                if cyclic(list(p)) == sorted(spectrum):
                     res.add(p)
 
-            elif m < parent and ok(p):
+            elif m < parent and ok(list(p)):
                 new.add(p)
 
         cand = new
 
     return res
 
+
 def to_letters(p):
     return "".join(mass_to_aa[x] for x in p)
 
-ans1 = solve()
-ans2 = cyclopeptide_sequencing(spectrum)
 
-print("Leaderboard:\n")
-for p in sorted(ans1):
-    print(to_letters(p))
+ans = solve()
 
-print("\nBrute Force:\n")
-for p in sorted(ans2):
+print("List of consistent 5-mers:\n")
+
+for p in sorted(ans):
     print(to_letters(p))
